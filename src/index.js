19f00ea8bda0c01daf2255f7a42742cb67336999
Shavanahola <kadisetramad.reizel@gmail.com>
2022-06-24 15:37:27 +0800

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Number([...Math.abs(n).toString()].reverse().join(""));
+    }
+    return Number([...n.toString()].reverse().join(""));
+};
